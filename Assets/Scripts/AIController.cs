0034d2c6c7ef2428d9a772a178da166823a0335b
lmkn02 <98485055+lmkn02@users.noreply.github.com>
2025-03-28 10:13:12 -0400

    done lab

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -3,4 +3,50 @@ using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
+ public StateMachine StateMachine { get; private set; }
+ public NavMeshAgent Agent { get; private set; }
+ public Animator Animator { get; private set; } // Not needed since we're not
+
+ public Transform[] Waypoints;
+ public Transform Player;
+ public float SightRange = 10f;
+ public float AttackRange = 2f;
+ public LayerMask PlayerLayer;
+ public StateType currentState;
+ void Start()
+ {
+ Agent = GetComponent<NavMeshAgent>();
+ Animator = GetComponent<Animator>(); 
+
+ StateMachine = new StateMachine();
+ StateMachine.AddState(new IdleState(this));
+ StateMachine.AddState(new PatrolState(this));
+ StateMachine.AddState(new ChaseState(this));
+ StateMachine.AddState(new AttackState(this)); 
+ StateMachine.TransitionToState(StateType.Idle);
+ }
+ void Update()
+ {
+ StateMachine.Update();
+ Animator.SetFloat("CharacterSpeed", Agent.velocity.magnitude);
+ currentState = StateMachine.GetCurrentStateType();
+ }
+ public bool CanSeePlayer()
+ {
+ float distanceToPlayer = Vector3.Distance(transform.position,
+Player.position);
+ if (distanceToPlayer <= SightRange)
+ {
+ // Optionally, add line of sight checks here using Raycast
+ return true;
+ }
+ return false;
+ }
+ // New method to check if the AI is within attack range
+ public bool IsPlayerInAttackRange()
+ {
+ float distanceToPlayer = Vector3.Distance(transform.position,
+Player.position);
+ return distanceToPlayer <= AttackRange;
+ }
 }
--- Assets/Scripts/State/IState.cs
+++ Assets/Scripts/State/IState.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public interface IState
 {
-   
+ StateType Type { get; }
+ void Enter();
+ void Execute();
+ void Exit();
 }
-
